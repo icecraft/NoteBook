71da69ed766406f178cde2938959426140987ac0
icecraft <tmortred@gmail.com>
2016-03-11 03:54:15 +0800

    采用 tornado 作为服务器

--- manage.py
+++ manage.py
@@ -4,6 +4,9 @@ from app import create_app, db
 from app.models import User, Note, Topic, Comment, Material
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
@@ -24,6 +27,11 @@ def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
+@manager.command
+def runtornado():
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(5050)
+    IOLoop.instance().start()
 
 if __name__ == '__main__':
     import sys
