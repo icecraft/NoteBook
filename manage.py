7bd8b77028cbb5268cd925a0f53c63e207402ab4
icecraft <tmortred@gmail.com>
2016-03-27 18:14:52 +0800

    modify config.py manage.py -- replace app with notebook

--- manage.py
+++ manage.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*
 import os
-from app import create_app, db
-from app.models import User, Note, Topic, Comment, Material
+from NoteBook import create_app
+from NoteBook import db as notebook_db
+from NoteBook.models import User, Note, Topic, Comment, Material
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-manager = Manager(app)
-migrate = Migrate(app, db)
-
+notebook = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(notebook)
+migrate = Migrate(notebook, notebook_db)
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Comment=Comment, Note=Note,\
+    return dict(app=app, notebook_db=notebook_db, User=User, Comment=Comment, Note=Note,\
                 Topic=Topic, Material=Material )
 manager.add_command("shell", Shell(make_context=make_shell_context))
-manager.add_command('db', MigrateCommand)
+manager.add_command('notebook_db', MigrateCommand)
+
 
 
 @manager.command
 def test():
     """Run the unit tests."""
     import unittest
-    tests = unittest.TestLoader().discover('tests')
+    tests = unittest.TestLoader().discover('NoteBook/tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 @manager.command
 def runtornado():
-    http_server = HTTPServer(WSGIContainer(app))
+    http_server = HTTPServer(WSGIContainer(notebook))
     http_server.listen(5050)
     IOLoop.instance().start()
 
